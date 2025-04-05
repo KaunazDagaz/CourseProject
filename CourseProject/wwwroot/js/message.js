4c4262ba58ff0ff3eab8f0d7a0645bdd3c2bd296
Mikita Besau <mikitabesau@gmail.com>
2025-04-05 13:37:26 +0300

    style: :lipstick: added proper table error displaying

---
+++ CourseProject/wwwroot/js/message.js
@@ -0,0 +1,18 @@
+﻿function showMessage(message, isError = true) {
+    const $toast = $('#messageToast');
+    const $messageBox = $('#messageBox');
+    $toast.removeClass('bg-success bg-danger text-white');
+    if (isError) {
+        $toast.addClass('bg-danger text-white');
+    } else {
+        $toast.addClass('bg-success text-white');
+    }
+    $messageBox.text(message);
+    const toastInstance = new bootstrap.Toast($toast[0], {
+        delay: 5000,
+        autohide: true
+    });
+    toastInstance.show();
+}
+
+window.showMessage = showMessage;
