6cd73aefa4c41bb9fa15cd1d6943bed3390eb62e
Mikita Besau <mikitabesau@gmail.com>
2025-04-13 14:39:44 +0300

    style: :lipstick: added themes support

---
+++ CourseProject/wwwroot/js/themeSwitcher.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const themeSwitcher = document.getElementById("theme-switcher");
+    const allTables = document.querySelectorAll("table");
+    function updateTableStyle(theme) {
+        allTables.forEach(table => {
+            if (theme === "solar") {
+                table.classList.add("table-dark");
+            } else {
+                table.classList.remove("table-dark");
+            }
+        });
+    }
+    const themeLink = document.getElementById("bootstrap-theme");
+    const savedTheme = localStorage.getItem("theme") || "zephyr";
+    themeLink.href = `/css/themes/theme-${savedTheme}.min.css`;
+    themeSwitcher.value = savedTheme;
+    updateTableStyle(savedTheme);
+
+    themeSwitcher.addEventListener("change", () => {
+        const selectedTheme = themeSwitcher.value;
+        updateTableStyle(selectedTheme);
+        themeLink.href = `/css/themes/theme-${selectedTheme}.min.css`;
+        localStorage.setItem("theme", selectedTheme);
+    });
+});
