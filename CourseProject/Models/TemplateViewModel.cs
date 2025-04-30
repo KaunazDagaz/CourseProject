2d68b6ed89a0340bbac89677dcf4b7827be7eb62
Mikita Besau <mikitabesau@gmail.com>
2025-04-30 18:31:24 +0300

    refactor: :recycle: cleared model files

--- CourseProject/Models/TemplateViewModel.cs
+++ CourseProject/Models/TemplateViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace CourseProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseProject.Models
 {
     public class TemplateViewModel
     {
@@ -9,5 +11,35 @@
         public required string Topic { get; set; }
         public string? Image { get; set; }
         public int LikesCount { get; set; }
+    }
+
+    public class TemplateGalleryViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required string AuthorName { get; set; }
+        public string? Image { get; set; }
+    }
+
+    public class TemplateTableViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string AuthorName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class TemplateCreateViewModel
+    {
+        [Required(ErrorMessage = "Error.TitleReuired")]
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "Error.TopicRequired")]
+        public required string Topic { get; set; }
+        public IFormFile? Image { get; set; }
+        [Required]
+        public bool IsPublic { get; set; } = true;
+        public string? Tags { get; set; }
     }
 }
