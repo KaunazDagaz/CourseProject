fc71eff15f102c5faabaebd30ff97b25ffd56984
Mikita Besau <mikitabesau@gmail.com>
2025-05-01 14:12:43 +0300

    feat: :sparkles: added personal page

---
+++ CourseProject/Models/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseProject.Models
+{
+    public class ProfileViewModel
+    {
+        public required string UserId { get; set; }
+        public required string Name { get; set; }
+        public List<TemplateTableViewModel> CreatedTemplates { get; set; } = new List<TemplateTableViewModel>();
+        public List<TemplateTableViewModel> FilledTemplates { get; set; } = new List<TemplateTableViewModel>();
+    }
+}
---
+++ CourseProject/wwwroot/js/templateTables.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    $(document).ready(function () {
+        if ($.fn.DataTable && $('#createdTemplatesTable').length) {
+            const createdTable = $('#createdTemplatesTable').DataTable({
+                "paging": false,
+                "info": false,
+                "searching": false,
+                "autoWidth": false,
+                "bJQueryUI": false,
+                "bAutoWidth": false,
+                "ordering": true,
+                "dom": 't',
+                "columnDefs": [
+                    { "orderable": true, "targets": [0, 1, 2] }
+                ],
+                "order": [[2, 'desc']]
+            });
+        }
+
+        if ($.fn.DataTable && $('#filledTemplatesTable').length) {
+            const filledTable = $('#filledTemplatesTable').DataTable({
+                "paging": false,
+                "info": false,
+                "searching": false,
+                "autoWidth": false,
+                "bJQueryUI": false,
+                "bAutoWidth": false,
+                "ordering": true,
+                "dom": 't',
+                "columnDefs": [
+                    { "orderable": true, "targets": [0, 1, 2] }
+                ],
+                "order": [[2, 'desc']]
+            });
+        }
+
+        $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+            $.fn.dataTable.tables({ visible: true, api: true }).columns.adjust();
+        });
+    });
+})();
